a099cebd46ffc39671f7283e3d8dde47b9281b6b
Isaac Redford <musepodcast@protonmail.com>
2025-08-22 22:59:30 -0500

    PATCH: Light/dark mode fixes and adjustments | tagalog, chinese simplified and tradtiional langauges have been fixed

--- podcast_web_app/podcasts/templates/account/email/email_confirmation_message.txt.py
+++
@@ -1,9 +0,0 @@
-BBBB BBBB
- gettext(u"You're almost done! Confirm your Muse Podcast account by clicking the link below:") SSSSSS SSSSSS SSSSS SSSSSSS SSSS SSSS SSSSSSS SSSSSSS SS SSSSSSSS SSS SSSS SSSSSS
-
-
-
- gettext(u'This link expires in 24 hours.') 
-
- gettext(u'Worm regards,') 
-XXXX XXXXXXX XXXX
